39e440dac950837dffb8912d5bbea6c833ba1513
Nikita Veretelnikov <verenick@mail.ru>
2021-04-10 14:39:35 +0300

    Added collision and a few bugs

---
+++ JS/csv.py
@@ -0,0 +1,3 @@
+import pandas as pd
+stuff = pd.read_csv('PACMAP.csv')
+display(stuff);
--- JS/game.js
+++ JS/game.js
@@ -1,6 +1,11 @@
 var pacman;
 var current_move_input = new Phaser.Math.Vector2(0, 0);
-var speed = 3;
+var speed = 2;
+var key_queue = Phaser.Input.Keyboard.KeyCodes.BACKSPACE;
+var map;
+var tileset;
+var layer;
+var tilesize = 48;
 class Game extends Phaser.Scene {
     constructor() {
         super();
@@ -14,15 +19,16 @@ class Game extends Phaser.Scene {
     }
     create() {
         this.physics.world.setBounds(0, 0, 800, 600);
-        var spriteBounds = Phaser.Geom.Rectangle.Inflate(
-            Phaser.Geom.Rectangle.Clone(this.physics.world.bounds), -100, -100);
-        var map = this.make.tilemap({ key: 'map', tileWidth: 48, tileHeight: 48 });
-        var tileset = map.addTilesetImage('tiles');
-        var layer = map.createLayer(0, tileset, 0, 0);
-        map.setCollisionBetween(54, 83);
-        pacman = this.physics.add.sprite(250, 250, 'pacman');
+        var spriteBounds = Phaser.Geom.Rectangle.Inflate(Phaser.Geom.Rectangle.Clone(this.physics.world.bounds), -100, -100);
+
+        map = this.make.tilemap({ key: 'map', tileWidth: tilesize, tileHeight: tilesize });
+        tileset = map.addTilesetImage('tiles', null, tilesize, tilesize);
+        layer = map.createLayer(0, tileset, 0, 0);
+
+
+        pacman = this.physics.add.sprite(tilesize + tilesize / 2, tilesize + tilesize / 2, 'pacman');
+        pacman.setScale(.5);
         pacman.setCollideWorldBounds(true);
-        this.physics.add.collider(pacman, layer);
         this.keys_arrows = this.input.keyboard.createCursorKeys();
         this.keys_wasd = this.input.keyboard.addKeys({
             'up': Phaser.Input.Keyboard.KeyCodes.W,
@@ -40,25 +46,89 @@ class Game extends Phaser.Scene {
         pacman.play('pacman');
     }
     update() {
+
         if (this.keys_arrows.up.isDown || this.keys_wasd.up.isDown) {
-            current_move_input = new Phaser.Math.Vector2(0, 0);
-            current_move_input.y = -1 * speed;
-            pacman.angle = 270;
+            key_queue = Phaser.Input.Keyboard.KeyCodes.W;
         } else if (this.keys_arrows.down.isDown || this.keys_wasd.down.isDown) {
-            current_move_input = new Phaser.Math.Vector2(0, 0);
-            current_move_input.y = 1 * speed;
-            pacman.angle = 90;
+            key_queue = Phaser.Input.Keyboard.KeyCodes.S;
         } else if (this.keys_arrows.right.isDown || this.keys_wasd.right.isDown) {
-            current_move_input = new Phaser.Math.Vector2(0, 0);
-            current_move_input.x = 1 * speed;
-            pacman.angle = 0;
+            key_queue = Phaser.Input.Keyboard.KeyCodes.D;
         } else if (this.keys_arrows.left.isDown || this.keys_wasd.left.isDown) {
-            current_move_input = new Phaser.Math.Vector2(0, 0);
-            current_move_input.x = -1 * speed;
-            pacman.angle = 180;
+            key_queue = Phaser.Input.Keyboard.KeyCodes.A;
         }
-        pacman.x += current_move_input.x;
-        pacman.y += current_move_input.y;
+        var margx = 0;
+        var margy = 0;
+        if (pacman.angle == -90) {
+            margy = -(tilesize / 2);
+        }
+        if (Math.abs(pacman.angle) == 180) {
+            margx = -(tilesize / 2);
+        }
+        if (pacman.angle == 90) {
+            margy = tilesize / 2;
+        }
+        if (pacman.angle == 0) {
+            margx = tilesize / 2;
+        }
+        // console.log(margx + " " + margy + " " + pacman.angle);
+        if (key_queue != Phaser.Input.Keyboard.KeyCodes.BACKSPACE) {
+            // if ((pacman.x - tilesize / 2) % tilesize == 0 && (pacman.y - tilesize / 2) % tilesize == 0) {
+            if (key_queue == Phaser.Input.Keyboard.KeyCodes.W) {
+                current_move_input = new Phaser.Math.Vector2(0, 0);
+                current_move_input.y = -1 * speed;
+                pacman.angle = 270;
+                margy = -(tilesize / 2);
+                margx = 0;
+            } else if (key_queue == Phaser.Input.Keyboard.KeyCodes.S) {
+                current_move_input = new Phaser.Math.Vector2(0, 0);
+                current_move_input.y = 1 * speed;
+                pacman.angle = 90;
+                margy = tilesize / 2;
+                margx = 0;
+            } else if (key_queue == Phaser.Input.Keyboard.KeyCodes.D) {
+                current_move_input = new Phaser.Math.Vector2(0, 0);
+                current_move_input.x = 1 * speed;
+                pacman.angle = 0;
+                margx = tilesize / 2;
+                margy = 0;
+            } else if (key_queue == Phaser.Input.Keyboard.KeyCodes.A) {
+                current_move_input = new Phaser.Math.Vector2(0, 0);
+                current_move_input.x = -1 * speed;
+                pacman.angle = 180;
+                margx = -(tilesize / 2);
+                margy = 0;
+            }
+            key_queue = Phaser.Input.Keyboard.KeyCodes.BACKSPACE;
+            // }
+        }
+        var tile = layer.getTileAtWorldXY(pacman.x + margx + current_move_input.x,
+            pacman.y + margy + current_move_input.y, true);
+        var tile1;
+        var tile2;
+        var tile3;
+        if (margx == 0) {
+            tile1 = layer.getTileAtWorldXY(pacman.x,
+                pacman.y + margy + current_move_input.y, true);
+            tile2 = layer.getTileAtWorldXY(pacman.x + tilesize / 2,
+                pacman.y + margy + current_move_input.y, true);
+            tile3 = layer.getTileAtWorldXY(pacman.x - tilesize / 2,
+                pacman.y + margy + current_move_input.y, true);
+        } else if (margy == 0) {
+            tile1 = layer.getTileAtWorldXY(pacman.x + margx + current_move_input.x,
+                pacman.y, true);
+            tile2 = layer.getTileAtWorldXY(pacman.x + margx + current_move_input.x,
+                pacman.y + tilesize / 2, true);
+            tile3 = layer.getTileAtWorldXY(pacman.x + margx + current_move_input.x,
+                pacman.y - tilesize / 2, true);
+        }
+        console.log(tile);
+        if (tile1 != null && tile2 != null && tile3 != null &&
+            tile1.index != 0 && tile2.index != 0 && tile3.index != 0) {
+            pacman.x += current_move_input.x;
+            pacman.y += current_move_input.y;
+        }
+        // if (pacman.x < tilesize / 2) pacman.x = tilesize / 2;
+        // if (pacman.y < tilesize / 2) pacman.y = tilesize / 2;
     }
 }
 const config = {
